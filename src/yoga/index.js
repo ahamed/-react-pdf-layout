fd260adf48f16f5d54652d0862d6ce49fbec3b31
Sajeeb Ahamed <sajeeb07ahamed@gmail.com>
2024-10-28 14:43:37 +0600

    Fix yoga issue

---
+++ src/yoga/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/prefer-default-export */
+
+import { loadYoga as yogaLoadYoga } from 'yoga-layout/load';
+
+let globalInstance;
+
+export const loadYoga = async () => {
+  // Yoga WASM binaries must be asynchronously compiled and loaded
+  // to prevent Event emitter memory leak warnings, Yoga must be loaded only once
+  const instance = await (globalInstance ??= yogaLoadYoga());
+
+  const config = instance.Config.create();
+
+  config.setPointScaleFactor(0);
+
+  const node = { create: () => instance.Node.createWithConfig(config) };
+
+  return { node };
+};
